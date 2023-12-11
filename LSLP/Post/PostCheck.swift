59c1c2d2e93e2c52d9fb3b6c4091aae01b15381a
Lavender <susie204@naver.com>
2023-12-12 03:37:50 +0900

    Post API 로직 구현, Model 구성

---
+++ LSLP/Post/PostCheck.swift
@@ -0,0 +1,8 @@
+//
+//  PostCheck.swift
+//  LSLP
+//
+//  Created by 이승현 on 12/12/23.
+//
+
+import Foundation
---
+++ LSLP/Post/PostModel.swift
@@ -0,0 +1,8 @@
+//
+//  PostModel.swift
+//  LSLP
+//
+//  Created by 이승현 on 12/12/23.
+//
+
+import Foundation
