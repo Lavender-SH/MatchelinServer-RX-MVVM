7ce01523725b0254f28fca3c3d320be65cbd5648
Lavender <susie204@naver.com>
2023-12-16 05:59:08 +0900

    프로필 화면 UI 구성

---
+++ LSLP/Profile API/ProfileModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileModel.swift
+//  LSLP
+//
+//  Created by 이승현 on 12/16/23.
+//
+
+import Foundation
---
+++ LSLP/Profile/ProfileView.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileView.swift
+//  LSLP
+//
+//  Created by 이승현 on 12/16/23.
+//
+
+import Foundation
